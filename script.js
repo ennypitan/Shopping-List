8318c166719238d3f353538b0708289c0454c873
ennypian adekoya <ennypitan@gmail.com>
2023-09-01 20:48:32 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
-const clearBtn = document.getElementById('clear')
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+
 
 
 function addItem(e){
@@ -25,11 +27,16 @@ function addItem(e){
     </button>`;
     itemList.appendChild(li);
     itemInput.value = '';
+    checkUI();
 }
 
 function removeItem(e){
     if(e.target.parentElement.classList.contains('remove-item')){
+       if(confirm('Are you sure')){
         e.target.parentElement.parentElement.remove();
+
+        checkUI();
+       }
     }
     
 }
@@ -40,8 +47,42 @@ function clearItems(){
         itemList.removeChild(itemList.firstChild);
 
     }
+    checkUI();
 }
+
+function checkUI() {
+    const items = itemList.querySelectorAll('li');
+  
+    if (items.length === 0) {
+      clearBtn.style.display = 'none';
+      itemFilter.style.display = 'none';
+    } else {
+      clearBtn.style.display = 'block';
+      itemFilter.style.display = 'block';
+    }
+  }
+
+
+  function filterItems(e) {
+    const items = itemList.querySelectorAll('li');
+    const text = e.target.value.toLowerCase();
+  
+    items.forEach((item) => {
+      const itemName = item.firstChild.textContent.toLowerCase();
+  
+      if (itemName.indexOf(text) != -1) {
+        item.style.display = 'flex';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
+
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
-clearBtn.addEventListener('click', clearItems)
+clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
